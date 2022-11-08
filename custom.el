1c4080fd15134d93b53ac32514f3c4f64ae53f64
Julio César <zzantares@gmail.com>
2022-11-08 00:10:46 -0600

    Initial commit

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((haskell-hoogle-url . "http://localhost:8123/?hoogle=%s")
+     (ormolu-process-path . "fourmolu")
+     (lsp-haskell-formatting-provider . "fourmolu")
+     (haskell-stylish-on-save)
+     (haskell-process-type . stack-ghci)
+     (haskell-indentation-starter-offset . 4)
+     (haskell-indentation-left-offset . 4)
+     (haskell-indentation-layout-offset . 4)
+     (web-mode-code-indent-offset . 2)
+     (web-mode-css-indent-offset . 2)
+     (web-mode-markup-indent-offset . 2))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "$HOME/.emacs.d" ]]; then
+    echo "You need to get Doom Emacs installed first, see: https://github.com/doomemacs/doomemacs#install"
+    exit 1
+fi
+
+echo "==> Linking Doom configuration ..."
+self=`realpath $0`
+ln -s `dirname $self` $HOME/.doom.d
+
+echo "==> Applying doom-colemak.patch ..."
+pushd $HOME/.emacs.d
+git apply $self/.doom.d/doom-colemak.patch
+popd
+
+echo "==> Done!"
